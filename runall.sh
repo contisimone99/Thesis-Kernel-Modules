082ab3baa656efcaf04a6dee4cf69b498621e009
Simone Conti <contisimone4@gmail.com>
2025-12-29 14:56:21 +0000

    updated runall.sh

--- runall.sh
+++ runall.sh
@@ -16,6 +16,7 @@ sudo find ./fx-module -name '*.ko' -exec cp "{}" "$MNT/root" \;
 sudo umount "$MNT"
 
 # Avvia QEMU
+
 sudo ~/qemu/build/qemu-system-x86_64 \
     -nographic \
     -device edu \
@@ -30,6 +31,5 @@ sudo ~/qemu/build/qemu-system-x86_64 \
     -s \
     -netdev user,id=network0,hostfwd=tcp::10022-:22 \
     -device e1000,netdev=network0,mac=52:54:00:12:34:56 \
-    -append "console=ttyS0 root=/dev/sda rw acpi=off nokaslr"
-    #-overcommit mem-lock=on \
-    #2>stderror_file \
+    -append "console=ttyS0 root=/dev/sda rw acpi=off nokaslr" \
+    #2>/tmp/qemu-kvm.log
